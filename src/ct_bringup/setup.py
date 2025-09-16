11de9ed47a25b92fe4253db0216b49aa0613b9c7
Galanti,Samuele S. <s.galanti@student.fontys.nl>
2025-09-16 09:28:56 +0200

    bringup readme.md + launch script

---
+++ src/ct_bringup/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+import glob
+package_name = 'ct_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']), 
+        #installing launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        #installing config files
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='samuele',
+    maintainer_email='s.galanti@student.fontys.nl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
