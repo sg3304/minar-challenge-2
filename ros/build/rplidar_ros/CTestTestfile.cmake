046c0f9779484132b27b366cf5be2704d6e1aa98
Galanti,Samuele S. <s.galanti@student.fontys.nl>
2025-09-23 12:05:23 +0200

    deleted build etc

--- ros/build/rplidar_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samuele/dev/minar/minar-challenge-2/ros/src/rplidar_ros
-# Build directory: /home/samuele/dev/minar/minar-challenge-2/ros/build/rplidar_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
