dc0ad626f1c1b3c6287bee2098c76bea92ab9011
Galanti,Samuele S. <s.galanti@student.fontys.nl>
2025-10-09 10:00:02 +0200

    praams

--- ros/src/ct_bringup/launch/bringup.launch.py
+++ ros/src/ct_bringup/launch/bringup.launch.py
@@ -58,7 +58,7 @@ def generate_launch_description():
     launch_arguments = {
         'use_sim_time': 'False',
         'use_localization': 'True',
-        'params_file': params_file
+        'params_file': nav2_params
     }
 
     # Only include the map argument if the map file exists
